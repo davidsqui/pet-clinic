da0f4f97485609948d9c6eed93a075109a34dd5f
davidsqui <davidsqr6@gmail.com>
2020-07-03 21:42:14 -0500

    refactoring to common crud service. closes #2

--- pet-clinic-data/src/main/java/com/dasc/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/dasc/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.dasc.petclinic.services;
 
 import com.dasc.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
